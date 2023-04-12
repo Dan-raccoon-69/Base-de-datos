7f1970f68ede2c68f69c3e028b30d7d83c4b5e91
Daniel <gustavodelacruz2010@gmail.com>
2023-04-11 18:03:58 -0600

    Cambios

--- BD Sakila/Consultas BD sakila.sql
+++ BD Sakila/Consultas BD sakila.sql
@@ -82,4 +82,73 @@ select * from store;
 select c.first_name as Nombre, a.address as Direccion , s.store_id as ID_Store from sakila.customer c left join sakila.store s 
 on (c.store_id = s.store_id) left join sakila.address a on (c.address_id = a.address_id);
 
+/* CONSULTAS COUNT, AVG, SUM, MAX, MIN. */
 
+/* Consulta la tabla rental de la base de datos sakila, 
+realiza un COUNT de  la columna rental_id */
+select * from rental;
+select count(rental_id) as Total_Registros from rental;
+
+/* Consulta la tabla payment de la base de datos sakila, 
+realiza un MAX de  la columna amount */
+select max(amount) as Cantidad_Maxima from payment;
+
+/* CONSULTAS CON GROUP BY. */
+
+/* Consulta la tabla actor de la base de datos sakila,
+realiza un conteo de la columna de cada last_name. */
+select last_name, count(last_name) from actor group by last_name;
+
+/* Consulta la tabla payment y  customer de la base de datos sakila,
+muestra en la tabla las columnas customer_id, first_name, last_name y la suma total de amount de cada customer_id */
+select * from payment;
+select * from customer;
+select c.customer_id as Id_Cliente, c.first_name as Nombre, c.last_name as Apellido, sum(p.amount) as Cantidad 
+from payment p inner join customer c on (c.customer_id = p.customer_id) group by c.customer_id;
+
+/* CONSULTAS CON HAVING. */
+
+/* Consulta la tabla payment y  customer de la base de datos sakila,
+muestra en la tabla las columnas customer_id, first_name, last_name y la suma total de amount de cada customer_id 
+cuando la suma total de amount sea < 80 y de acuerdo al campo amount ordenalos de forma descendente */
+select c.customer_id as Id_Cliente, c.first_name as Nombre, c.last_name as Apellido, sum(p.amount) as Cantidad 
+from payment p inner join customer c on (c.customer_id = p.customer_id) group by c.customer_id
+having sum(p.amount) < 80 order by sum(p.amount) desc;
+
+/* Consulta la tabla actor de la base de datos sakila, 
+realiza un COUNT de  last_name, mostrar cuando el COUNT sea mayor de 2 */
+select * from actor;
+select last_name as Apellido, count(last_name) as Conteo_Apellidos from actor 
+group by last_name having count(last_name) > 2;
+
+/* Más CONSULTAS */
+
+-- ¿Qué actores tienen el primer nombre 'Scarlett'?
+select * from actor where first_name = 'Scarlett';
+
+-- ¿Qué actores tienen el apellido "Johansson"?
+select * from actor where last_name = 'Johansson';
+
+-- ¿Cuántos apellidos de actores distintos hay?
+select distinct(last_name) from actor;
+
+-- ¿Qué apellidos no se repiten?
+select last_name from actor group by last_name having count(last_name) < 2;
+
+-- ¿Qué actor ha aparecido en la mayoría de las películas?
+select * from film_actor;
+select * from film;
+select * from actor;
+
+select fa.actor_id, a.first_name, a.last_name, count(fa.actor_id) from film_actor fa
+inner join film f on (fa.film_id = f.film_id)
+inner join actor a on (a.actor_id = fa.actor_id) 
+group by actor_id 
+order by count(actor_id) desc
+limit 1;
+
+-- ¿Se puede alquilar ‘Academy Dinosaur’ en la tienda 1?
+-- Conociendo que film_id = 1 = Academy Dinosaur.
+select f.film_id, f.title, i.store_id from film f 
+inner join inventory i on (f.film_id = i.film_id) 
+having f.film_id = 1;
